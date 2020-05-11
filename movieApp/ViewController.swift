e0a9dc7da09b53f20a15ebc140b18413241fb51a
Mattia La Spina <mattiaa95@gmail.com>
2020-05-11 14:36:31 +0100

    Initial Commit

---
+++ movieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by Casita on 5/11/20.
+//  Copyright © 2020 Mattia La Spina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
